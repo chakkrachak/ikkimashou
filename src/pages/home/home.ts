138413e234cf482d1b0bd02eaeebed631d6cfb29
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-26 17:25:45 +0100

    Moving some code to controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    lines = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    stopPoints = ['Stop Point 1', 'Stop Point 2'];
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
 
 }
